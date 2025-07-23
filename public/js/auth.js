163488c07ddb7eb6836394886b799d5b096547b8
yustfeda <warungdm1@gmail.com>
2025-07-23 09:39:22 +0700

    update

--- public/js/auth.js
+++ public/js/auth.js
@@ -14,7 +14,7 @@ document.getElementById('loginForm').addEventListener('submit', (e) => {
         })
         .catch((error) => {
             // Gagal login
-            errorMessage.textContent = 'Login gagal'
+            errorMessage.textContent = 'Login gagal: ' + error.message;
         });
 });
 
